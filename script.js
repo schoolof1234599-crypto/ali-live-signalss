37dfde4c2a0bd3fa884fb953f0236834ec9145e1
schoolof1234599-crypto <schoolof1234599@gmail.com>
2025-12-25 08:29:27 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 fetch("signals.json")
-  .then(res => res.json())
-  .then(data => {
-    const s = data[0];
+.then(res => res.json())
+.then(data => {
+  const box = document.getElementById("signals");
+  box.innerHTML = "";
 
-    document.getElementById("signals").innerHTML = `
-      <h2>${s.type} - ${s.pair}</h2>
-      <p>Entry: ${s.entry}</p>
-      <p>TP: ${s.tp}</p>
-      <p>SL: ${s.sl}</p>
-      <b>${s.note}</b>
+  data.forEach(s => {
+    box.innerHTML += `
+      <div class="card">
+        <h2 class="${s.type === 'BUY' ? 'buy' : 'sell'}">
+          ${s.type} - ${s.pair}
+        </h2>
+        <p>Entry: ${s.entry}</p>
+        <p>TP: ${s.tp}</p>
+        <p>SL: ${s.sl}</p>
+        <p class="note">${s.note}</p>
+      </div>
     `;
   });
+});
